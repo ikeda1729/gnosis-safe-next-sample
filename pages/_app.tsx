a046137de01d0e6dbbacb77c3b945f2f351a60f5
ikeda <d.enkikairo@gmail.com>
2023-03-28 12:15:50 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
+import SafeProvider from '@safe-global/safe-apps-react-sdk'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SafeProvider>
+      <Component {...pageProps} />
+    </SafeProvider>
+  )
 }
